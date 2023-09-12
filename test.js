e376454774e02b74760d3fd232df14b9a0baa177
harpreet kaur <harpreetvirdi17594@gmail.com>
2023-09-12 20:13:30 +0100

    updated index.html file

--- test.js
+++ test.js
@@ -86,61 +86,82 @@ const askGenQuestion = async () => {
       //send to classes
       // const obj = new Employee(engAnswers.engName, engAnswers.engID);
       // obj.break;
-      fs.writeFile(
-        "index.html",
-        `
-        <!DOCTYPE html>
-        <html lang="en">
-        
-        <head>
-            <meta charset="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-            <title>My Team</title>
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-                integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-            <link rel="stylesheet" href="style.css">
-            <script src="https://kit.fontawesome.com/c502137733.js"></script>
-        </head>
-        
-        <body>
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-12 jumbotron mb-3 team-heading">
-                        <h1 class="text-center">My Team</h1>
-                    </div>
-                </div>
-            </div>
-            <div class="container">
-                <div class="row">
-                    <div class="team-area col-12 d-flex justify-content-center">
-                    <div class="card employee-card">
-                    <div class="card-header bg-primary">
-                        <h2 class="card-title">${engAnswers.engName}</h2>
-                        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${answers.options}</h3>
-                    </div>
-                    <div class="card-body">
-                        <ul class="list-group">
-                            <li class="list-group-item">ID:${engAnswers.engId}</li>
-                            <li class="list-group-item">Email:${engAnswers.engEmail}</li>
-                            <li class="list-group-item">GitHub:${engAnswers.engGithub}</li>
-                        </ul>
-                    </div>
-                </div>
-                    </div>
-                </div>
-            </div>
-        </body>
-        </html>
-        
-        
-        
-        
-        
-        `,
 
-        (err) => (err ? console.error(err) : console.log("file updated!!!!"))
-      );
+      //for second input
+      const answers1 = await inquirer.prompt(questions);
+
+      switch (answers1.options) {
+        case "intern":
+          internAnswers = await inquirer.prompt(internQuestions);
+          fs.writeFile(
+            "index.html",
+            `
+            <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>My Team</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/c502137733.js"></script>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 jumbotron mb-3 team-heading">
+                <h1 class="text-center">My Team</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="team-area col-12 d-flex justify-content-center">
+            <div class="card employee-card">
+            <div class="card-header bg-primary">
+                <h2 class="card-title">${internAnswers.internName}</h2>
+                <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${answers1.options}</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${internAnswers.internId}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${internAnswers.internEmail}">${internQuestions.internGithub}</a></li>
+                    <li class="list-group-item">Github: ${internAnswers.internGithub}</li>
+
+                    </ul>
+            </div>
+        </div>
+        <div class="card employee-card">
+    <div class="card-header bg-primary">
+        <h2 class="card-title">${engAnswers.engName}</h2>
+        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${answers.options}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${engAnswers.engId}</li>
+            <li class="list-group-item">Email: <a href="mailto:${engAnswers.engEmail}"></a></li>
+            <li class="list-group-item">Github: ${engAnswers.engGithub}</li>
+        </ul>
+    </div>
+</div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+              
+              
+              
+            `,
+
+            (err) =>
+              err ? console.error(err) : console.log("file updated!!!!")
+          );
+      }
       break;
     case "intern":
       internAnswers = await inquirer.prompt(internQuestions);
